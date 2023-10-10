871cdde06f9046faacd2ca45c4e01d23a70a004f
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-11 01:53:10 +0300

    Task 1 done

---
+++ 0x02-functions_nested_loops/1-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - A program that prints the alphabet followed by a new line.
+ * Return: void here
+*/
+
+void print_alphabet(void)
+{
+    char c = 'a';
+while (c >= 'a' && c <= 'z')
+{
+    putchar(c);
+    c++;
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 * print_alphabet - Function that prints alphabet 10 times
 * Return: void here
 */
+void print_alphabet(void);
 void print_alphabet_x10(void);
