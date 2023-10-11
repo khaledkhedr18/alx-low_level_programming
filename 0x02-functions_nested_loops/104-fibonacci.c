ddebbb7d563c8f35604d9990a3f21863cda13126
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-11 05:33:06 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
     int count;
     unsigned long f1 = 0, f2 = 1, sum;
-    unsigned long f1h1, f1h2, f2h1, f2h2;  // Renamed the variables
+    unsigned long f1h1, f1h2, f2h2;  /* Renamed the variables */
     unsigned long h1, h2;
 
     for (count = 0; count < 92; count++)
@@ -38,8 +38,7 @@ int main(void)
             printf(", ");
         f1h1 = f2h2;
         f1h2 = f2h2;
-        f2h1 = h1;  // Updated this variable
-        f2h2 = h2;
+        f2h2 = h1;
     }
     printf("\n");
     return 0;
