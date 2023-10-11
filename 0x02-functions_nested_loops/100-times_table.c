36a62edb261fedccbb2c9ca392316f8b41d83ed3
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-11 05:01:16 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,32 +6,32 @@
   */
 void print_times_table(int n)
 {
-	int n, m, p;
+	int number, multiply, product;
 
 	if (n >= 0 && n <= 15)
 	{
-	for (n = 0; n <= n; n++)
+	for (number = 0; number <= n; number++)
 	{
 	_putchar('0');
-	for (m = 1; m <= n; m++)
+	for (multiply = 1; multiply <= n; multiply++)
 	{
 	_putchar(',');
 	_putchar(' ');
-	p = n * m;
-	if (p <= 99)
+	product = number * multiply;
+	if (product <= 99)
 	_putchar(' ');
-	if (p <= 9)
+	if (product <= 9)
 	_putchar(' ');
-	if (p >= 100)
+	if (product >= 100)
 	{
-	_putchar((p / 100) + '0');
-	_putchar(((p / 10)) % 10 + '0');
+	_putchar((product / 100) + '0');
+	_putchar(((product / 10)) % 10 + '0');
 	}
-	else if (p <= 99 && p >= 10)
+	else if (product <= 99 && product >= 10)
 	{
-	_putchar((p / 10) + '0');
+	_putchar((product / 10) + '0');
 	}
-	_putchar((p % 10) + '0');
+	_putchar((product % 10) + '0');
 	}
 	_putchar('\n');
 	}
