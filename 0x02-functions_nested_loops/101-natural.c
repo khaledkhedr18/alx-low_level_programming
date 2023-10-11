762ee3e8c621ffdb2faa35c72c701c2d58a289bf
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-11 05:20:47 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
-
-int main()
+/**
+* main - Entry point
+* Return: Always 0.
+*/
+int main(void)
 {
 int sum = 0;
 int i;
 int reversed = 0;
 
-	for (i = 1; i < 1024; i++) {
-			if (i % 3 == 0 || i % 5 == 0) {
-			sum += i;
-		}
+	for (i = 1; i < 1024; i++)
+	{
+	if (i % 3 == 0 || i % 5 == 0) {
+	sum += i;
+	}
 	}  
     if (sum == 0) {
-		putchar('0');
+	putchar('0');
 	}
 	else {
-        if (sum < 0) {
-            putchar('-');
-            sum = -sum;
-        }
-
-     
-        while (sum > 0) {
-            reversed = reversed * 10 + (sum % 10);
-            sum /= 10;
-        }
-        
-        while (reversed > 0) {
-            putchar('0' + (reversed % 10));
-            reversed /= 10;
-        }
-    }
-
-  
-    putchar('\n');
-
-    return 0;
+		if (sum < 0)
+		{
+			putchar('-');
+			sum = -sum;
+		}
+		while (sum > 0)
+		{
+		reversed = reversed * 10 + (sum % 10);
+		sum /= 10;
+		}
+		while (reversed > 0)
+		{
+		putchar('0' + (reversed % 10));
+		reversed /= 10;
+		}
+		}
+		putchar('\n');
+		return 0;
 }
