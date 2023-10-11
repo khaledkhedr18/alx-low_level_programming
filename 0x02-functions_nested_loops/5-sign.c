678bf09260f11387cf3bcc823524697295fe8a91
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-11 03:34:27 +0300

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_sign - Entry point
+* Description: A function that prints the sign of a number
+* @n: the character
+* Return: 1-if positive, 0-if zero, -1-if negative
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	putchar(45);
+	return(-1);
+	}
+	else
+	{
+	putchar('0');
+	return (0);
+	}
+}
