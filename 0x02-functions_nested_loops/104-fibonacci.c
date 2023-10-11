24b6eac9c3a003536331f746e53f582ceabc5cb2
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-11 05:49:08 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - The main function
  * Prints the Fibonacci sequence up to a certain limit
@@ -11,7 +10,6 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib2_half1, fib1_half2, fib2_half2;
 	unsigned long half1, half2;
-
 	for (count = 0; count < 92; count++)
 	{
 	sum = fib1 + fib2;
@@ -23,29 +21,23 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%010lu", half1, half2);
-
 		if (count != 98)
 		printf(", ");
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-
 	printf("\n");
 	return (0);
 	}
