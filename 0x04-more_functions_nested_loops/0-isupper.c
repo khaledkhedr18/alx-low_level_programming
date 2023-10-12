206ac3e6159acc8652170f6c53e8f5b2951957c0
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 02:27:16 +0300

    a lot of updates

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* _isupper - Entry point
+* Return: always 0.
+* @c: testing placeholder
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is the folder of 5th topic we're going to address this week.
+let's go, alx!!
