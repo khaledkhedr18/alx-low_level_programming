1d2d296deb803ac0da8c0c9c5b88af36d2afc479
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 02:31:42 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - Entry point
+* Return: always 0.
+* @c, @b: testing placeholders
+*/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+
+}
