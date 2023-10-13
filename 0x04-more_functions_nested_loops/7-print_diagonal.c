e3deabde462c0ab27638f0dad10ff33f5922d4ab
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 04:44:56 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,13 +19,13 @@ int i, j;
 	{
 		for (i = 0; i <= n; i++)
 		{
-				for (j = 0; j <= n; j++)
-				{
-					if (j == i)
-					putchar('\\');
-					else if (j < i)
-					putchar(' ');
-				}
+			for (j = 0; j <= n; j++)
+			{
+			if (j == i)
+			putchar('\\');
+			else if (j < i)
+			putchar(' ');
+			}
 		putchar('\n');
 		}
 	}
