e3586b16ecb117adccaee24c149c4a9c29064a44
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 03:52:36 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,18 +4,19 @@
 /**
 * print_line - main function
 * Return: void.
+* @n: testing placeholder parameter
 */
 
 void print_line(int n)
 {
 int lines = 0;
-	if(n <= 0)
+	if (n <= 0)
 	{
 	putchar('\n');
 	}
 else
 {
-	while(lines <= n)
+	while (lines <= n)
 	{
 	putchar(95);
 	lines++;
