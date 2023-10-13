f7370fad2c8bc0f9ccbc1a5e52fdedb6c21a97d6
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 05:31:52 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,25 @@ int j;
 	for (j = 1; j <= 100; j++)
 	{
 	if (j % 3 == 0 && j % 5 == 0)
+	{
 	printf(" FizzBuzz");
+	}
 	else if (j % 5 == 0 && j % 3 != 0)
+	{
 	printf(" Buzz");
+	}
 	else if (j % 3 == 0 && j % 5 != 0)
+	{
 	printf(" Fizz");
+	}
 	else if (j ==1)
+	{
 	printf("%d", j);
+	}
+	else
+	{
+	printf(" %d", j);
+	}
 	}
 printf("\n");
 return (0);
