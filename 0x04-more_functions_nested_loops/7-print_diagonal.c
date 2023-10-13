64911caa0533b51c093bc01b634b214cc906ac71
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 04:43:00 +0300

    task 8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_diagonal - main function
+* Return: void.
+* @n: testing placeholder parameter
+*/
+
+void print_diagonal(int n)
+{
+int i, j;
+
+	if (n <= 0)
+	{
+	putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+				{
+					if (j == i)
+					putchar('\\');
+					else if (j < i)
+					putchar(' ');	
+				}
+		putchar('\n');
+		}
+	}
+}
