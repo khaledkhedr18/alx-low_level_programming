51d990e6727bace3cc4f87c3f5ffcfb370912c83
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 05:09:48 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+int j;
+
+	for (j = 1; j <= 100; j++)
+	{
+	if (j % 3 == 0 && j % 5 == 0)
+	printf("FizzBuzz ");
+	else if (j % 5 == 0)
+	printf("Buzz ");
+	else if (j % 3 == 0)
+	printf("Fizz ");
+	else
+	printf("%d ", j);
+	}
+return (0);
+}
