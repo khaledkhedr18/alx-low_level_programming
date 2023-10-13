954ac2586969c0560f8267e85953b15bcb0c6616
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-13 03:09:45 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,17 @@ void print_most_numbers(void)
 {
 	int a;
 	int i;
-	for(i = 0; i <=14; i++)
-{
-	for (a = 0; a <= 14; a++)
+
+	for (i = 0; i <= 14; i++)
 	{
-		if (a >= 10)
-	{
-	putchar('1');
-	}
-	putchar(a % 10 + '0');
-	}
+		for (a = 0; a <= 14; a++)
+		{
+			if (a >= 10)
+			{
+			putchar('1');
+			}
+		putchar(a % 10 + '0');
+		}
 	putchar('\n');
-}
+	}
 }
