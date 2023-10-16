e6ea5c323d73114a003ebe064b955c23c810dd02
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-17 00:47:26 +0300

    task 4

--- trashtest/arithmetic.c
+++ trashtest/arithmetic.c
@@ -1,36 +1,11 @@
 #include <stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always (0)
-*/
 
-int main(void)
-{
-int a = 21;
-int b = 10;
-int c;
+void _puts(char *str) {
+        puts(str);
+}
 
-c = a + b;
-printf("Line 1 Value of c: %d \n", c);
-
-c = a - b;
-printf("Line 2 value of c: %d \n", c);
-
-c = a * b;
-printf("Line 3 value of c: %d \n", c);
-
-c = a / b;
-printf("Line 4 value of c: %d \n", c);
-
-c = a % b;
-printf("Line 5 value of c: %d \n", c);
-
-c = a++;
-printf("Line 6 value of c: %d \n", c);
-
-c = a--;
-printf("Line 7 value of c: %d \n", c);
-
-return (0);
+int main() {
+    char *text = "Hello, World!";
+    _puts(text);
+    return 0;
 }
