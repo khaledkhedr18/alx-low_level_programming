63b15a0e8a4a31165954cf7bdee459f13afd0fd7
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-17 02:19:22 +0300

    task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,22 +2,22 @@
 #include "main.h"
 
 /**
-* _puts2 - a function that prints string
+* puts2 - a function that prints string
 * @str: a string placeholder
 * Return: void
 */
 
 void puts2(char *str)
 {
+	int i;
 
 	if (str != NULL)
 	{
-		while (*str != '\0')
-			{
-			putchar(*str);
-			str = str + 2;
-			}
-
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (i % 2 == 0)
+			putchar(str[i]);
+		}
 		putchar('\n');
 	}
 }
