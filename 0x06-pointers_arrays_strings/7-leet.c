3f03ccff2a543384d9f128de08a1e7d020be3b3c
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-18 20:15:15 +0300

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+* leet - a function that prints string
+* @str: a string placeholder
+* Return: void
+*/
+
+char *leet(char *str)
+{
+	int i;
+	int j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '0'; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
