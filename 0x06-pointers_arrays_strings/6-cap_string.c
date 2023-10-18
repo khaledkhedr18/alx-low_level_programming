1e565bc2e3fce7468fd8fa536893ff856fef29a1
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-19 01:30:19 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,26 @@
 * Return: void
 */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int i;
 	int j;
 	char sep[] = {',', ';', '.', '!', '?',
 	'"', '(', ')', '}', '{', ' ', '\n', '\t'};
 
-	if (s != NULL)
+	if (str != NULL)
 	{
-		for (i = 0; s[i] != '\0'; i++)
+		for (i = 0; str[i] != '\0'; i++)
 		{
 			for (j = 0; sep[j] != '\0'; j++)
-			if (s[i] == sep[j])
+			if (str[i] == sep[j])
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{
-				s[i + 1] = toupper(s[i + 1]);
+				str[i + 1] = toupper(str[i + 1]);
 				}
 			}
 		}
 	}
-	return (s);
+	return (str);
 }
