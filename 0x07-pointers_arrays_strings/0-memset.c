68c64eb55614fcf071c1ff54c66bfe3eb9ba89ab
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-23 17:54:33 +0300

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+* _memset - this function fills memory witha constant bite
+* return: char
+* @s: the address of the memory needed to be filled
+* @b: the value filled with
+* @n: the number of bytes to be set to the value
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	if (s != NULL)
+	{
+		return (memset(s, b, n));
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+this is the third time and hopefully the last time that we're going to be dealing with pointers and arrays
+let's see how it goes!!
