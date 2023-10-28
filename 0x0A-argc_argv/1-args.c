42f9dc5a59a6cc2caa69ab04d9e24cba8850ed7d
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-10-28 04:29:36 +0200

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - a function that prints it's file name
+* Return: 0.
+* @argc: number of arguments
+* @argv: an array of strings that has the name of the arguments
+*/
+
+int main(int argc, char const *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
