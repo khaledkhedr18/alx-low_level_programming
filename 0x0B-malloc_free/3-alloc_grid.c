b0611ca828e9672796dda78a5e4fdff7bb9693e8
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-01 03:58:09 +0200

    ss

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,28 +13,28 @@ int **alloc_grid(int width, int height)
 	int **t;
 	int i, j;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
+	t = malloc(sizeof(*t) * height);
 
-	t = malloc(height * sizeof(int *));
-	if (t == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
+	if (width <= 0 || height <= 0 || t == 0)
 	{
-		t[i] = malloc(width * sizeof(int));
-		if (t[i] == NULL)
-		{
-			/* Free previously allocated rows */
-			for (j = 0; j < i; j++)
-				free(t[j]);
-			free(t);
-			return (NULL);
-		}
-		free(t[i]);
-		for (j = 0; j < width; j++)
-			t[i][j] = 0;
+		return (NULL);
 	}
-	free(t);
+	else{
+		for (i = 0; i < height; i++)
+		{
+			t[i] = malloc(sizeof(**t) * width);
+			if (t[i] == 0)
+			{
+				while (i--)
+				free(t[i]);
+				free(t);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			{
+				t[i][j] = 0;
+			}
+		}
+	}
 	return (t);
 }
