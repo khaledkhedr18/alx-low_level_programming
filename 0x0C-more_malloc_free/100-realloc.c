7f0218aadd506a4e367aa92762c26c5305971f6c
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-03 02:59:05 +0200

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,39 +13,41 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr = malloc(new_size);
+	void *ptrs;
+	unsigned int i;
 
-if (new_size == old_size)
-{
-return (ptr);
-}
+	if (new_size == old_size)
+	return (ptr);
 
-if (ptr == NULL)
-{
-	return (malloc(new_size));
-}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL)
+	}
+	if (ptr == NULL)
+	{
+		ptrs = malloc(new_size);
+		if (ptrs == NULL)
+		{
+			return (NULL);
+		}
+		return (ptrs);
+	}
 
-if (new_size == 0)
-{
-free(ptr);
-return (NULL);
-}
+	if (new_size > old_size)
+	{
+		ptrs = malloc(new_size);
 
-if (new_ptr == NULL)
-{
-return (NULL);
+		if (ptrs == NULL)
+		{
+			return (NULL);
+		}
+		
+		for (i = 0; i < old_size && i < new_size; i++)
+		{
+			*((char *)ptrs + i) = *((char *)ptr + i);
+		}
+		free(ptr);
+	}
+	return (ptrs);
 }
-
-if (new_size > old_size)
-{
-memcpy(new_ptr, ptr, old_size);
-}
-else
-{
-memcpy(new_ptr, ptr, new_size);
-}
-
-free(ptr);
-return (new_ptr);
-}
-
