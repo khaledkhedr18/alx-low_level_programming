2415cf81791f1ec62e951004b69698c79de57757
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-07 01:53:36 +0200

    ss

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* struct dog - main structure
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+structuring our way through.
