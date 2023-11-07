bf1619b0fb89ea4f084ef4ab711335b4dca29120
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-07 03:02:34 +0200

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+* new_dog - main structure
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *dog;
+    if (!name || age < 0 || !owner)
+	{
+        return (NULL);
+    }
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+	return (NULL);
+
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}
