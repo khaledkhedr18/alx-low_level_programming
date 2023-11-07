3ddcfba19708290931a6c8489f2b95f6a87cc6e0
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-07 03:08:15 +0200

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,35 +12,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
+dog_t *dog;
 
-    if (!name || age < 0 || !owner)
-    {
-        return (NULL);
-    }
+if (!name || age < 0 || !owner)
+{
+return (NULL);
+}
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
 
-    dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-    strcpy(dog->name, name);
+dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+strcpy(dog->name, name);
 
-    dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
-    strcpy(dog->owner, owner);
+dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+strcpy(dog->owner, owner);
 
-    dog->age = age;
+dog->age = age;
 
-    return (dog);
+return (dog);
 }
