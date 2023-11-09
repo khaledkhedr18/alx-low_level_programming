361008b61873cab83cfcfc536b3875dc1eb6e3c8
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-09 03:34:47 +0200

    ss

---
+++ 0x0F-function_pointers/3-op_functionc.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - main function
+* Return: int
+* @a: 1st number
+* @b: 2nd number
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - main function
+* Return: int
+* @a: 1st number
+* @b: 2nd number
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - main function
+* Return: int
+* @a: 1st number
+* @b: 2nd number
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - main function
+* Return: int
+* @a: 1st number
+* @b: 2nd number
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - main function
+* Return: int
+* @a: 1st number
+* @b: 2nd number
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
