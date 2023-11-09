fb3588aa6005e1c055e6c9e7a3b2512cf751ebec
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-09 03:40:33 +0200

    ss

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 	b = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
-	if(!op_func)
+	if (!op_func)
 	printf("Error\n"), exit(99);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
