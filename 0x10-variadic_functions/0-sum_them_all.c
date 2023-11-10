bd031fb79a8e6c533058537177d085829018cbaf
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-10 03:22:46 +0200

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,16 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int sum = 0;
 
 	va_list nptr;
+
 	va_start(nptr, n);
 
 	if (n == 0)
-	return 0;
+	return (0);
 
 	for (i = 0; i < n; i++)
 	{
 		unsigned int x = va_arg(nptr, int);
+
 		sum += x;
 	}
 	return (sum);
