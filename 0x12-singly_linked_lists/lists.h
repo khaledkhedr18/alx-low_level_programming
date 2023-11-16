88dbd295253be2c0def9e77bfbc5ab4b7ea69f4f
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-16 22:40:29 +0200

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
 
 typedef struct list_t *nodePtr;
 
-typedef struct list_t 
+typedef struct list_t
 {
 	char *str;
 	unsigned int len;
 	nodePtr next;
-}list_t;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
