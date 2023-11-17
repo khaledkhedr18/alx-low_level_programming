5ff2675250140d2250c748ea3cb8efec56b06a1a
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-17 04:39:31 +0200

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* list_len - a function that returns the number of elements in a linked list
+* Return: the number of elements.
+* @h: a counter to the linked lists
+*/
+
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return 0;
+	}
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return count;
+}
