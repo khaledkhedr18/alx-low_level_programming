665b96d4bcca016615d5d9744f2243dbcbc08ea7
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-24 04:25:04 +0200

    task 0

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,12 +10,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int mask = 1 << index;
+
 if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
 
-unsigned long int mask = 1 << index;
 *n |= mask;
 
 return (1);
