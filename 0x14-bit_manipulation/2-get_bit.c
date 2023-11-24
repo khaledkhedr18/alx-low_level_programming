b72a6f0652b786ce2276965d8cd0c681009bd9cb
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-24 04:14:41 +0200

    task 0

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,9 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-	{
-		return (-1);
-	}
-
-	unsigned long int mask = 1UL << index;
-	int bit_value = (n & mask) != 0 ? 1 : 0;
-
-	return (bit_value);
+    if (index >= sizeof(unsigned long int) * 8)
+    {
+        return (-1);
+    }
+    return (n >> index) & 1;
 }
