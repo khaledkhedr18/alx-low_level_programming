2745da5c1d46404b61e5d2c4a08553cbf5e1abf7
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-24 04:31:15 +0200

    task 0

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* flip_bits - main function
+* Return: int
+* @n: an integer
+* @m: an int
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor_result = n ^ m;
+unsigned int count = 0;
+
+while (xor_result != 0)
+{
+if (xor_result & 1)
+count++;
+xor_result >>= 1;
+}
+
+return count;
+}
