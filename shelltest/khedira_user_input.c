69de71faddff63480fde56efed10eb6a77c50b68
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-06 16:01:41 +0200

    ss

--- shelltest/khedira_user_input.c
+++ shelltest/khedira_user_input.c
@@ -1,5 +1,12 @@
 #include "shell_header_file.h"
 
+/**
+* khedira_user_input - a function that receives an input from the user
+* Return: void
+* @comm: the command that the user is going to enter
+* @size: the size of the string of the input
+*/
+
 void khedira_user_input(char *comm, size_t size)
 {
 	if (fgets(comm, size, stdin) == NULL)
