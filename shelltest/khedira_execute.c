ddfb2f02efff8618ff8a4ec26c74bec0bc36a878
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-06 14:55:07 +0200

    ss

--- shelltest/khedira_execute.c
+++ shelltest/khedira_execute.c
@@ -1,5 +1,10 @@
 #include "shell_header_file.h"
-#include <unistd.h>
+
+/**
+* executioner - a function that executes arguments or commands
+* Return: Void
+* @exec_comm: string argument
+*/
 
 void executioner(const char *exec_comm)
 {
@@ -11,4 +16,15 @@ void executioner(const char *exec_comm)
 		exit(EXIT_FAILURE);
 	}
 	
+	else if (child_processid == 0)
+	{
+		execlp(exec_comm, exec_comm, (char*)NULL);
+		perror("exelcp");
+		exit(EXIT_FAILURE);
+	}
+
+	else
+	{
+		wait(NULL);
+	}
 }
