cdb456d6a0314d97bd31b2042424dccb4275ac6c
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-06 12:45:26 +0200

    2nd update to the shell

--- shelltest/khedira_execute.c
+++ shelltest/khedira_execute.c
@@ -3,5 +3,12 @@
 
 void executioner(const char *exec_comm)
 {
-	pid_t child_pid = fork();
+	pid_t child_processid = fork();
+
+	if (child_processid == -1)
+	{
+		perror("fork failed!");
+		exit(EXIT_FAILURE);
+	}
+	
 }
