895e1ef1dbc3acb4fa85c5f0d3ee9ec9c3ad0043
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-08 22:25:51 +0200

    ss

--- shell2/shell_headerrr.h
+++ shell2/shell_headerrr.h
@@ -11,8 +11,9 @@
 #include <sys/wait.h>
 
 void khedira(const char *output);
-void khedira_uinput(const char *comm, size_t size_t);
+void khedira_uinput(char *comm, size_t size_t);
 void khedira_prmpt();
+void execute_command(const char *exec_comm, char *const exec_args[]);
 
 
 #endif
