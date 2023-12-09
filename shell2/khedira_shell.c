14e456d6dd1e01ced04086fa765a50ea15585965
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-09 18:33:18 +0200

    ss

--- shell2/khedira_shell.c
+++ shell2/khedira_shell.c
@@ -7,15 +7,15 @@
 * @argv: the strings of the arguments
 */
 
-int main(int ac, char **av)
+int main(int ac, char *argv[])
 {
-	void(ac);
 	char khedira_prmpt[128];
 
 	while (true)
 	{
 		khedira_prompt();
 		khedira_uinput(khedira_prmpt, sizeof(khedira_prmpt));
-		khedira_exec(khedira_prmpt, av);
+		khedira_exec(khedira_prmpt, argv);
 	}
+	return (0);
 }
