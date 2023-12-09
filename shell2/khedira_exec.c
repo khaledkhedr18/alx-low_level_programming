74bc9381fa951bb7224c75f39f2b6e0a94b8fc93
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-09 17:54:13 +0200

    ss

--- shell2/khedira_exec.c
+++ shell2/khedira_exec.c
@@ -1,38 +1,38 @@
-// #include "shell_headerrr.h"
+#include "shell_headerrr.h"
 
-// /**
-//  * khedira_exec - a function that executes commands
-//  * Return: Void
-//  * @exec_comm: the command to be executed
-//  * @exec_args: the arguments to the command
-// */
+/**
+ * khedira_exec - a function that executes commands
+ * Return: Void
+ * @exec_comm: the command to be executed
+ * @exec_args: the arguments to the command
+*/
 
 
-// void khedira_exec(const char *exec_comm, char *const exec_args[])
-// {
-// pid_t child_processid = fork();
+void khedira_exec(const char *exec_comm, char *const exec_args[])
+{
+pid_t child_processid = fork();
 
-// if (child_processid == -1)
-// 	{
-// 		perror("fork");
-// 		exit(EXIT_FAILURE);
-// 	}
+if (child_processid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 
-// 	else if (child_processid == 0)
-// 	{
-// 		if (execvp(exec_comm, exec_args) == -1)
-// 		{
-// 			perror("execvp");
-// 			exit(EXIT_FAILURE);
-// 		}
-// 	}
-// 	else
-// 	{
-// 		int status;
-// 		if (wait(&status) == -1)
-// 		{
-// 			perror("wait");
-// 		exit(EXIT_FAILURE);
-// 		}
-// 	}
-// }
+	else if (child_processid == 0)
+	{
+		if (execvp(exec_comm, exec_args) == -1)
+		{
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		int status;
+		if (wait(&status) == -1)
+		{
+			perror("wait");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
