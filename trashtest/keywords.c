43fa72f0cf938339a9e377ae5846a081677089bc
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-02-25 23:11:01 +0200

    asddd

--- trashtest/keywords.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-/**
-* Keywords:
-* int, float, break, goto, if, for, continue,while, do
--- trashtest/printmyname.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-	printf("my name is khaled khedr");
-	return (0);
-}
